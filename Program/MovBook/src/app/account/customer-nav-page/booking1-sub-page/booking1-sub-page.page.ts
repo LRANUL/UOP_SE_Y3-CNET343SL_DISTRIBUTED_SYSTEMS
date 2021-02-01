1c7d6ab3e550d2540c22a7ed88e6e638a288334a
nnwijesuriya <51411336+nnwijesuriya@users.noreply.github.com>
2021-02-01 17:36:56 +0530

    Customer - 'Booking1' - sub page (v1u3) - 10674049)

--- Program/MovBook/src/app/account/customer-nav-page/booking1-sub-page/booking1-sub-page.page.ts
+++ Program/MovBook/src/app/account/customer-nav-page/booking1-sub-page/booking1-sub-page.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-booking1-sub-page',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Booking1SubPagePage implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
+    this.startTimer();
   }
+
+  counter: {
+    min,
+    sec
+  } 
+
+  startTimer() {
+    this.counter = { min: 10, sec: 0  } // Put any time you want
+    let intervalId = setInterval(() => {
+      if (this.counter.sec - 1 == -1) {
+        this.counter.min -= 1;
+        this.counter.sec = 59;
+      } 
+      else this.counter.sec -= 1
+
+    if (this.counter.min === 0 && this.counter.sec == 0)
+    {
+       this.route.navigateByUrl(''); // put the url you want to navigate to
+       this.counter = { min: 10, sec: 0  }
+    }
+    }, 1000)
+  }
+
 
 }
