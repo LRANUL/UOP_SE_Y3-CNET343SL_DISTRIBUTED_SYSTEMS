c7237b3f7a6a3ddc212a80f46661c434b0f4ee56
IshanC7 <49109028+TheNooobC7@users.noreply.github.com>
2021-02-20 23:17:42 +0530

    server v1(u4) -10673936
    Configuring users model and adding the mongoose-unique validator  (npm install --save mongoose-unique-validator)

--- Program/MovBook-Backend/models/users.js
+++ Program/MovBook-Backend/models/users.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
-const userSchema =mongoose.Schema({
-  email: {type : String, required:true },
+const userSchema =mongoose.Schema({ // removed the required in some fields for testing purposes 
+  email: {type : String, required:true , unique:true},
   password: {type : String, required:true },
-  name: {type : String, required:true },
-  status: {type : String, required:true },
-  type: {type : String, required:true },
-  address: {type : String, required:true },
-  phone: {type : String, required:true }
+  name: {type : String  }, 
+  status: {type : String},
+  type: {type : String  },
+  address: {type : String},
+  phone: {type : String }
 })
 
 
-
+userSchema.plugin(uniqueValidator);
 module.exports = mongoose.model('Users',userSchema); 
