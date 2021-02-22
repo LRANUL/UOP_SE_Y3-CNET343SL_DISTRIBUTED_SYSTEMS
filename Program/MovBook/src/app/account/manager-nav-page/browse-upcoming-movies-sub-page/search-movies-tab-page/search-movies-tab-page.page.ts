88bd8d843f1de7691a204534a0641ff2024761df
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2021-02-22 08:31:35 +0530

    Manager - 'Browse Upcoming Movies' UI - Sub Page (v1u7) - 10673969
    - Added a loading spinner to activate until the movie results are returned.
    - Implemented mechanism to show custom text block if no cover image was returned in the movie results.
    - Added text block to activate if no movie results are available for the provided movie preferences.

--- Program/MovBook/src/app/account/manager-nav-page/browse-upcoming-movies-sub-page/search-movies-tab-page/search-movies-tab-page.page.ts
+++ Program/MovBook/src/app/account/manager-nav-page/browse-upcoming-movies-sub-page/search-movies-tab-page/search-movies-tab-page.page.ts
@@ -1,3 +1,4 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AlertController, ModalController } from '@ionic/angular';
@@ -13,9 +14,20 @@ import { MovieDetailsModalPage } from '../movie-details-modal/movie-details-moda
 })
 export class SearchMoviesTabPagePage implements OnInit {
 
+  // Declaration - formgroup to collect user preferences to search movies on
   searchUpcomingMoviesForm: FormGroup;
 
-  public movieSearchResults: MovieSearchResult;
+  // Declaration - stores returned movie search results using user defined 'MovieSearchResult' object data type
+  movieSearchResults: MovieSearchResult;
+
+  // Declaration | Initialization - to handle visibility of 'noOfResultsFoundText' block
+  noOfResultsFoundText: Boolean = false;
+
+  // Declaration | Initialization - to handle visibility of 'noAvailableMovieText' block
+  noAvailableMovieText: Boolean = false;
+
+  // Declaration | Initialization - to handle visibility of 'loadingSpinnerSearchUpcomingMovies' block
+  loadingSpinnerSearchUpcomingMovies: Boolean = false;
   
   constructor(
     private managerService: ManagerService,
@@ -26,6 +38,7 @@ export class SearchMoviesTabPagePage implements OnInit {
 
   ngOnInit() {
 
+    // Assigning form validation
     this.searchUpcomingMoviesForm = this.formBuilder.group({
       movieTitle: new FormControl('', Validators.required),
       movieReleaseYear: new FormControl('')
@@ -33,14 +46,77 @@ export class SearchMoviesTabPagePage implements OnInit {
 
   }
 
+  // Alert Box Implementation
+  async alertNotice (title: string, content: string) {
+    const alert = await this.alertController.create({
+      header: title,
+      message: content,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
   // Retrieving movie search results from the backend by passing the enter movie title and movie release data
   getMovieSearchResults(formValue){
+
+    // Resetting boolean values to initial state
+
+    // Disabling visibility of no available movie text 
+    this.noAvailableMovieText = false;
+
+    // Disabling visibility of number of results found text
+    this.noOfResultsFoundText = false;
+
+
+
+    // Enabling visibility of loading spinner
+    this.loadingSpinnerSearchUpcomingMovies = true;
+
+    // Send request to server-side and get search movie results as a response
     this.managerService.getMovieSearchResults(formValue.movieTitle, formValue.movieReleaseYear)
       .subscribe((movieSearchResults: MovieSearchResult) => {
+
+        // Disabling visibility of loading spinner
+        this.loadingSpinnerSearchUpcomingMovies = false;
+        
+        // If condition - checking whether the returned response is equal to false
+        if(movieSearchResults.Response == "False"){
+
+          // Enabling visibility of no available movie text 
+          this.noAvailableMovieText = true;
+
+          // Disabling visibility of number of results found text
+          this.noOfResultsFoundText = false;
+
+        }
+        else{
+
+          // Disabling visibility of no available movie text 
+          this.noAvailableMovieText = false;
+
+          // Enabling visibility of number of results found text
+          this.noOfResultsFoundText = true;
+
+        }
+        // Initializing retrieved values into user defined 'movieSearchResults' variable 
         this.movieSearchResults = movieSearchResults;
+        console.log(this.movieSearchResults);
       },
       (error: ErrorEvent) => {
-        alert(error);
+
+        // Disabling visibility of loading spinner
+        this.loadingSpinnerSearchUpcomingMovies = false;
+
+        // Disabling visibility of no available movie text 
+        this.noAvailableMovieText = false;
+
+        // Disabling visibility of number of results found text
+        this.noOfResultsFoundText = false;
+
+        // Showing error message box to the user
+        this.alertNotice("ERROR", "Unable to retrieve result(s), apologies for the inconvenience. Please contact administrator.");
+
+        console.log("Unable to retrieve results: ", error);
       });
   }
 
