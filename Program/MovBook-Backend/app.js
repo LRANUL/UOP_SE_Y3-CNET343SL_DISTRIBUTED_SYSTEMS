fef244bec0203b4effc13dd74f2c607c46278d9c
IshanC7 <49109028+TheNooobC7@users.noreply.github.com>
2021-03-14 13:27:02 +0530

    10673936 - Operator login

--- Program/MovBook-Backend/app.js
+++ Program/MovBook-Backend/app.js
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With, Content-Type, Accept"
+    "Origin,X-Requested-With, Content-Type, Accept,authorization"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
