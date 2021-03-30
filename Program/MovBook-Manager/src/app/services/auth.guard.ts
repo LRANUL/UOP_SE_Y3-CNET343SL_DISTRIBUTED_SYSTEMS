5364fbc33f94d428953a7bb6bd559147352cf92e
IshanC7 <49109028+TheNooobC7@users.noreply.github.com>
2021-03-30 15:55:07 +0530

    Manager - Login -10673936

---
+++ Program/MovBook-Manager/src/app/services/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from "./auth.service";
+
+@Injectable()
+export class AuthGuard implements CanActivate{
+  constructor(private authService:AuthService, private router:Router){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    const isAuth = this.authService.getAuthStatus();
+    if(!isAuth){
+      this.router.navigate(['/']);
+    }
+    return isAuth;
+  }
+
+}
