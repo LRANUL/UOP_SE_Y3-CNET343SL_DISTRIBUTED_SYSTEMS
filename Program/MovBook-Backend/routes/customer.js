4515d2b9c323c790270ce812a67bcaea616489cb
Ranul Ladduwahetty <10673986@students.plymouth.ac.uk>
2021-04-03 23:08:57 +0530

    Server v1u6 - 10673986
    - Bug on user update fixed

--- Program/MovBook-Backend/routes/customer.js
+++ Program/MovBook-Backend/routes/customer.js
@@ -6,50 +6,43 @@ const jwt = require("jsonwebtoken");
 const Users = require("../models/customers");
 
 //get user data with id
-router.get('/:email' ,(req,res,next)=>{
-  Users.findOne({email: req.params.email})
-    .then((data)=>{
+router.get('/:email', (req, res, next) => {
+  Users.findOne({ email: req.params.email })
+    .then((data) => {
       console.log(data);
-      if(data)
-    {
-      res.status(200).json({
-        message: "It works",
-        users: data
-      })
-    }else
-    {
-      res.status(404).json({
-        message: "The user does not exist"
-      })
-    } 
+      if (data) {
+        res.status(200).json({
+          message: "It works",
+          users: data
+        })
+      } else {
+        res.status(404).json({
+          message: "The user does not exist"
+        })
+      }
     }).catch(err => {
-     console.log(err);
+      console.log(err);
     })
 });
 
 //update the user 
-router.put('/:id' ,(req,res,next)=>{
+router.put('/:id', (req, res, next) => {
   console.log(req.body);
-  Users.updateOne({email: req.params.id}, 
+  Users.updateOne({ email: req.params.id },
     {
-    name:
-    {
-      prefix: req.body.prefix,
-      firstName: req.body.firstName,
-      middleName: req.body.middleName,
-      lastName: req.body.lastName
-    }, 
-    address:
-    {
-      streetAddress: req.body.streetAddress,
-      city: req.body.city,
-      postalZipCode: req.body.postalZipCode
-    },
-    phone: req.body.phone 
+      'name.0.prefix': req.body.prefix,
+      'name.0.firstName': req.body.firstName,
+      'name.0.middleName': req.body.middleName,
+      'name.0.lastName': req.body.lastName,
+      'address.0.streetAddress': req.body.streetAddress,
+      'address.0.city': req.body.city,
+      'address.0.postalZipCode': req.body.postalZipCode,
+      "phone": req.body.phone
     })
-  .then((data) => {
-    console.log(data);
-  })
+    .then((data) => {
+      console.log(data);
+      res.send("Updated")
+    })
 });
 
 module.exports = router;
