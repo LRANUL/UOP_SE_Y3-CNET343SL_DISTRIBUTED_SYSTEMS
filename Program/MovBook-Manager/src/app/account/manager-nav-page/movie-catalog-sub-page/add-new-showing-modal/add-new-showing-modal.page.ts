b94c8cc403b28a4007b05e22e868041a5d3a75e6
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2021-04-08 01:22:58 +0530

    Manager - 'Movie Catalog' - Sub Page (v1u4) - 10673969
    - Updated 'Add New Showing' modal user interface.

--- Program/MovBook-Manager/src/app/account/manager-nav-page/movie-catalog-sub-page/add-new-showing-modal/add-new-showing-modal.page.ts
+++ Program/MovBook-Manager/src/app/account/manager-nav-page/movie-catalog-sub-page/add-new-showing-modal/add-new-showing-modal.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AlertController, ModalController } from '@ionic/angular';
+import { ManagerService } from 'src/app/services/account/manager.service';
 
 @Component({
   selector: 'app-add-new-showing-modal',
@@ -8,12 +10,31 @@ import { AlertController, ModalController } from '@ionic/angular';
 })
 export class AddNewShowingModalPage implements OnInit {
 
+  // Declaration - FormGroup to handle addNewShowingForm form
+  addNewShowingForm: FormGroup;
+
   constructor(
     private modalController: ModalController,
+    private formBuilder: FormBuilder,
+    private managerService: ManagerService,
     private alertController: AlertController
   ) { }
 
   ngOnInit() {
+
+    // Assigning form validation to AddNewShowingForm
+    this.addNewShowingForm = this.formBuilder.group({
+      cinemaLocation: new FormControl('', Validators.required),
+      cinemaHall: new FormControl('', Validators.required),
+      showingPeriodStartDate: new FormControl('', Validators.required),
+      showingPeriodEndDate: new FormControl('', Validators.required),
+      timeSlotStartTime: new FormControl('', Validators.required),
+      timeSlotEndTime: new FormControl('', Validators.required),
+      ticketCostChild: new FormControl('', Validators.required),
+      ticketCostAdult: new FormControl('', Validators.required),
+      showingExperience: new FormControl('', Validators.required)
+    });
+
   }
 
   // Implementation to close 'Add New Showing' modal
