c30d963b89083ab442755b8c63b759450ca5a83f
Ranul Ladduwahetty <10673986@students.plymouth.ac.uk>
2021-04-08 20:46:24 +0530

    Server v1u7 - 10673986
    - Solution modified

--- Program/MovBook-Backend/routes/customer.js
+++ Program/MovBook-Backend/routes/customer.js
@@ -30,18 +30,24 @@ router.put('/:id', (req, res, next) => {
   console.log(req.body);
   Users.updateOne({ email: req.params.id },
     {
-      'name.0.prefix': req.body.prefix,
-      'name.0.firstName': req.body.firstName,
-      'name.0.middleName': req.body.middleName,
-      'name.0.lastName': req.body.lastName,
-      'address.0.streetAddress': req.body.streetAddress,
-      'address.0.city': req.body.city,
-      'address.0.postalZipCode': req.body.postalZipCode,
+      name:[ {
+        "prefix":req.body.prefix,
+        "firstName":req.body.firstName,
+        'middleName':req.body.firstName,
+        'lastName':req.body.lastName,
+      }],
+      address:[
+        {
+          'streetAddress':req.body.streetAddress,
+          'city':req.body.city,
+          'postalZipCode':req.body.postalZipCode,
+        }
+      ],
       "phone": req.body.phone
     })
     .then((data) => {
       console.log(data);
-      res.send("Updated")
+      res.send(data)
     })
 });
 
