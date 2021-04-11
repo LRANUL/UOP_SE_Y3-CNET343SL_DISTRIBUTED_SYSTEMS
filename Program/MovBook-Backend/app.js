93dd5e448769497870b594fa63b8e27cc01f55d5
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2021-04-11 10:11:01 +0530

    Manager - 'Locations-Halls' (v1u10) - Sub Page - 10673969
    - Added functionality to show number of cinema halls for each cinema location.
    - Added functionality to search cinema locations.

--- Program/MovBook-Backend/app.js
+++ Program/MovBook-Backend/app.js
@@ -46,8 +46,8 @@ mongoose
     console.log("Connection Failed");
   });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
