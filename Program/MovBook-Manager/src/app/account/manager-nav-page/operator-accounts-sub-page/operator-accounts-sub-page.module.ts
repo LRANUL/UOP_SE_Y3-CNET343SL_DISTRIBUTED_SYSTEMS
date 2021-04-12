56be77b236e3763bcd656eafb5eb037f67744d37
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2021-04-13 01:17:24 +0530

    Manager - 'Operator-Accounts' - Sub Page - 10673969
    - Added frontend (user interface) in 'Operator Accounts' sub page.

--- Program/MovBook-Manager/src/app/account/manager-nav-page/operator-accounts-sub-page/operator-accounts-sub-page.module.ts
+++ Program/MovBook-Manager/src/app/account/manager-nav-page/operator-accounts-sub-page/operator-accounts-sub-page.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,6 +13,7 @@ import { OperatorAccountsSubPagePage } from './operator-accounts-sub-page.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     OperatorAccountsSubPagePageRoutingModule
   ],
   declarations: [OperatorAccountsSubPagePage]
--- Program/MovBook-Manager/src/app/account/manager-nav-page/operator-accounts-sub-page/operator-accounts-sub-page.page.ts
+++ Program/MovBook-Manager/src/app/account/manager-nav-page/operator-accounts-sub-page/operator-accounts-sub-page.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
+import { ManagerService } from 'src/app/services/account/manager.service';
+import { MovieDetailsModalPage } from '../browse-upcoming-movies-sub-page/movie-details-modal/movie-details-modal.page';
 
 @Component({
   selector: 'app-operator-accounts-sub-page',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OperatorAccountsSubPagePage implements OnInit {
 
-  constructor() { }
+  // Declaration - FormGroup to collect user preferences to search operator account
+  searchOperatorAccountForm: FormGroup;
+
+  // TEMPORARY Declaration - Handle section visibility
+  operatorAccountDetails = {
+    operatorAccountView: "Initial", // Initial, Found and NotAvailable
+    operatorAccountStatus: "null" // Disabled, Enabled and null
+  };
+
+  constructor(
+    private managerService: ManagerService,
+    private modalController: ModalController,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+
+    // Assigning 'searchOperatorAccountForm' form validation
+    this.searchOperatorAccountForm = this.formBuilder.group({
+      emailAddress: new FormControl('', Validators.required),
+      namePrefix: new FormControl(''),
+      firstName: new FormControl(''),
+      lastName: new FormControl('')
+    });
+
+  }
+
+  // Function - Implementation for opening the 'Register Operator Account' modal
+  async openRegisterOperatorAccountModal(accountDetails: any){
+    const registerOperatorAccountModal = await this.modalController.create({
+      component: MovieDetailsModalPage,
+      componentProps: {
+        passingOperatorDetails: "<PASS USER OBJECT>"
+      },
+      // Disabling modal closing from any outside clicks
+      backdropDismiss: false,
+    });
+    registerOperatorAccountModal.present();
   }
 
 }
