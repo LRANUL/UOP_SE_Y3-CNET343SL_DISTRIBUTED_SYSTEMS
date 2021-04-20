a40602b59037abb0cba7d29fa5236fcd11a9a772
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2021-04-21 00:07:08 +0530

    Manager - Removed Duplicate Files - 10673969
    - Removed all duplicate files in the 'MovBook' app.
    - 'MovBook-Manager' app will be the main desktop application for the manager end user.

--- Program/MovBook/src/app/app-routing.module.ts
+++ Program/MovBook/src/app/app-routing.module.ts
@@ -28,10 +28,6 @@ const routes: Routes = [
   //   loadChildren: () => import('./account/administrator-nav-page/administrator-nav-page.module').then( m => m.AdministratorNavPagePageModule)
   // },
   // {
-  //   path: 'manager',
-  //   loadChildren: () => import('./account/manager-nav-page/manager-nav-page.module').then( m => m.ManagerNavPagePageModule)
-  // },
-  // {
   //   path: 'operator',
   //   loadChildren: () => import('./account/operator-nav-page/operator-nav-page.module').then( m => m.OperatorNavPagePageModule)
   // },
--- Program/MovBook/src/app/app.module.ts
+++ Program/MovBook/src/app/app.module.ts
@@ -11,12 +11,6 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { MovieDetailsModalPageModule } from './account/manager-nav-page/browse-upcoming-movies-sub-page/movie-details-modal/movie-details-modal.module';
-import { MovieCatalogTypesPopoverPageModule } from './account/manager-nav-page/browse-upcoming-movies-sub-page/movie-catalog-types-popover/movie-catalog-types-popover.module';
-import { AddHallModalPageModule } from './account/manager-nav-page/locations-halls-sub-page/add-hall-modal/add-hall-modal.module';
-import { AssignHallSeatPopoverPageModule } from './account/manager-nav-page/locations-halls-sub-page/assign-hall-seat-popover/assign-hall-seat-popover.module';
-import { AddLocationModalPageModule } from './account/manager-nav-page/locations-halls-sub-page/add-location-modal/add-location-modal.module';
-import { CinemaHallsModalPageModule } from './account/manager-nav-page/locations-halls-sub-page/cinema-halls-modal/cinema-halls-modal.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxStripeModule } from 'ngx-stripe';
@@ -28,12 +22,6 @@ import { NgxStripeModule } from 'ngx-stripe';
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
-    MovieCatalogTypesPopoverPageModule,
-    MovieDetailsModalPageModule,
-    AddHallModalPageModule,
-    AssignHallSeatPopoverPageModule,
-    AddLocationModalPageModule,
-    CinemaHallsModalPageModule,
     BrowserAnimationsModule,
     NgbModule,
     NgxStripeModule.forRoot('pk_test_51IEFJWJcuxwrCkGRQaDQ8ycbMSxyrsApmMnXmb9Zlnvo3rcws57a5fRf8gdD6n1meV9yH8KctT2OUbN8kfRka2hi00T1GM5O0W')
